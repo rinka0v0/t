d4f5ac1eb7fe521cbc44116727f7379550e8bade
rinka0v0 <rinka0v0@users.noreply.github.com>
2022-01-01 17:41:07 +0900

    環境構築

---
+++ deep_dive/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "./src/index.ts",
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  // import 文で .ts ファイルを解決するため
+  // これを定義しないと import 文で拡張子を書く必要が生まれる。
+  // フロントエンドの開発では拡張子を省略することが多いので、
+  // 記載したほうがトラブルに巻き込まれにくい。
+  resolve: {
+    // 拡張子を配列で指定
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 4000,
+    open: true,
+    hot: true,
+  },
+};
